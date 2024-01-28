cb247175e9e24e47b0a6981ed5f3d35d30000e19
noobed_max <146673372+noobed-max@users.noreply.github.com>
2024-01-28 23:11:41 +0530

    Update setup.py

--- client/setup.py
+++ client/setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='s1_client',
+    name='s1_client_test',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        'uvicorn',
+        'requests',
+        'python-multipart',
+        'fastapi',
+    ],
     entry_points={
         'console_scripts': [],
     },
