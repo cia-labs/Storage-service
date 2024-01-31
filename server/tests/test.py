4780ea2652b0eae64634fe0f4d9660168828a874
Ashish Yadav <ayadav9738@gmail.com>
2024-01-31 22:47:49 +0530

    modified:   server/tests/test.py

--- server/tests/test.py
+++ server/tests/test.py
@@ -104,3 +104,15 @@ def update(API_URL, key,directory_path, new_key=None):
 
     except requests.HTTPError as e:
         print("Error:", e)
+
+def delete(API_URL, key):
+    try:
+        response = requests.delete(f"{API_URL}/delete/?key={key}")
+
+        if response.status_code == 200:
+            print("Update successful:", response.json())
+        else:
+            raise requests.HTTPError(response.text)
+
+    except requests.HTTPError as e:
+        print("Error:", e)
