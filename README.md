d15432f8cffa2749e5b45ccce17af941eb2cdc1d
Ashish Yadav <ayadav9738@gmail.com>
2024-02-19 15:20:47 +0530

    modified:   .gitignore 	modified:   README.md 	new file:   server/__init__.py 	new file:   server/app/crud/crud.py 	modified:   server/main.py 	new file:   server/tests/test.py

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # CIAOS - CIA's Object Store
-Multi node, General purpose KV Store. 
+
+Multi node, General purpose KV Store.
 
 HLD - High Level Design
 https://drive.google.com/file/d/1BWJzX_X4IUtnwxcTL0QxThmeq6kVCHHz/view?usp=drive_link
-
 
 Client SDK
 
@@ -14,39 +14,60 @@ pip install ciaos
 ```
 
 ## Usage
+
 ### SAVE
+
 ```
 from ciaos import save
 
 save(API_URL, key, value)
 ```
+
 - API_URL: the url to the fastapi server
 - key: the key value which can be used to identify your content in Database
 - Value: the Base64 encoded binary data that you want to save / upload to database
+
 ---
-Note: A random Key will be generated , if no key is passed to save parameters 
----
+
+## Note: A random Key will be generated , if no key is passed to save parameters
+
 ### GET
+
 ```
 from ciaos import get
 
 get(API_URL, key)
 ```
+
 - API_URL: the url to the fastapi server
 - key: the key value which can be used to identify and fetch the data from Database
 
 ### UPDATE
+
 ```
 from ciaos import update
 
-update(API_URL,key,value,new_key) 
+update(API_URL,key,value,new_key)
 ```
-- NOTE: The new_key is an optional parameter. To be used if and only if you want to update the key. 
 
+- NOTE: The new_key is an optional parameter. To be used if and only if you want to update the key.
 
 ### DELETE
+
 ```
 from ciaos import delete
 
 delete(API_URL,key)
+```
+
+### Server
+
+```
+API SPECIFICATION
+
+POST /upload/: Upload files to the server.
+GET /retrieve/{key}: Retrieve files from the server.
+PUT /update/: Update files on the server.
+DELETE /delete/: Delete files from the server
+
 ```
---
+++ server/tests/test.py
@@ -0,0 +1,97 @@
+from fastapi.testclient import TestClient
+from server.main import app 
+
+client = TestClient(app)
+
+def test_upload_success():
+    key = None  
+    encoded_content = ["encoded_content_1", "encoded_content_2"]
+    
+    response = client.post("/update_file/", data={"key": key, "encoded_content": encoded_content})
+
+    assert response.status_code == 200
+
+    response_data = response.json()
+    generated_key = response_data.get("key", None)
+    assert response_data == {"message": "File uploaded successfully", "key": generated_key}
+
+def test_upload_fail():
+    response = client.post(
+        "/update_file/",
+        data={"key": "testkey", "encoded_content": []}
+    )
+    print(response.json())
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "type": "missing",
+                "loc": ["body", "encoded_content"],
+                "msg": "Field required",
+                "input": None,
+                "url": "https://errors.pydantic.dev/2.5/v/missing"
+            }
+        ]
+    }
+
+def test_retrieve_file_success():
+
+    key = "test_key"
+    encoded_content = ["encoded_content_1", "encoded_content_2"]
+    client.post("/update_file/", data={"key": key, "encoded_content": encoded_content})
+
+    response = client.get(f"/get/{key}")
+    print(response.status_code)
+    print(response.content)
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_retrieve_file_key_not_found():
+    non_existent_key = "nonexistentkey"
+
+    response = client.get(f"/get/{non_existent_key}")
+
+    print(response.status_code)
+    print(response.content)
+
+    assert response.status_code == 500
+    assert response.json() == {'detail': '404: Key not found'}
+
+
+def test_update_files_success():
+    key = "test_key"
+    new_key = "new_test_key"
+    encoded_content = ["encoded_content_1", "encoded_content_2"]
+
+    response = client.post("/update_file/", data={"key": key, "encoded_content": encoded_content})
+    assert response.status_code == 200
+
+    response = client.put(f"/update/?key={key}&new_key={new_key}", data={"encoded_content": encoded_content})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Files updated successfully"}
+
+def test_update_files_no_key_found():
+    key = "non_existent_key"
+    new_key = "new_test_key"
+    encoded_content = ["encoded_content_1", "encoded_content_2"]
+
+    response = client.put(f"/update/?key={key}&new_key={new_key}", data={"encoded_content": encoded_content})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Key not found"}
+
+def test_delete_files_success():
+    key = "test_key"
+    encoded_content = ["encoded_content_1", "encoded_content_2"]
+
+    client.post("/update_file/", data={"key": key, "encoded_content": encoded_content})
+
+    response = client.delete(f"/delete/?key={key}")
+    assert response.status_code == 200
+    assert response.json() == {"message": f"Folder '{key}' and its contents deleted successfully"}
+
+def test_delete_files_no_key_found():
+    non_existent_key = "non_existent_key"
+
+    response = client.delete(f"/delete/?key={non_existent_key}")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Folder not found"}
