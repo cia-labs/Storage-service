1034882f64538971f74fe7f8f21d9346642fd9cc
noobed-max <siddiq@student.cialabs.tech>
2024-03-25 21:48:51 +0530

    resolved issues 1

--- server/tests/test.py
+++ server/tests/test.py
@@ -12,7 +12,8 @@ from main import app
 
 client = TestClient(app)
 
-def test_upload_success():
+def test_upload_retrieve():
+    #test for upload sucess
     key = None  
     encoded_content = ["encoded_content_1", "encoded_content_2"]
     
@@ -24,6 +25,7 @@ def test_upload_success():
     generated_key = response_data.get("key", None)
     assert response_data == {"message": "File uploaded successfully", "key": generated_key}
 
+    #test for failue of upload
     response = client.post(
         "/upload/",
         data={"key": "test", "encoded_content": []}
@@ -41,7 +43,7 @@ def test_upload_success():
             }
         ]
     }
-
+    # test for retreive 
     key = generated_key
 
     response = client.get(f"/get/{key}")
@@ -50,6 +52,7 @@ def test_upload_success():
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
+#test for failure of retrieve
     non_existent_key = "nonexistentkey"
 
     response = client.get(f"/get/{non_existent_key}")
@@ -59,40 +62,29 @@ def test_upload_success():
 
     assert response.status_code == 500
     assert response.json() == {'detail': '404: Key not found'}
-
-
-def test_update_files_success():
-    key = "test_key1"
-    new_key = "new_test_key1"
-    encoded_content = ["encoded_content_1", "encoded_content_2"]
-
-    response = client.post("/upload/", data={"key": key, "encoded_content": encoded_content})
-    assert response.status_code == 200
+#test for update sucess
+    key = "test_key"
+    new_key = "new_test_key"
+    encoded_content = ["encoded_content_3", "encoded_content_4"]
 
     response = client.put(f"/update/?key={key}&new_key={new_key}", data={"encoded_content": encoded_content})
     assert response.status_code == 200
     assert response.json() == {"message": "Files updated successfully"}
-
-def test_update_files_no_key_found():
-    key = "non_existent_key2"
-    new_key = "new_test_key2"
-    encoded_content = ["encoded_content_1", "encoded_content_2"]
+#test for failure of update
+    key = "non_existent_key"
+    new_key = "new_key"
+    encoded_content = ["encoded_content_3", "encoded_content_4"]
 
     response = client.put(f"/update/?key={key}&new_key={new_key}", data={"encoded_content": encoded_content})
     assert response.status_code == 404
     assert response.json() == {"detail": "Key not found"}
-
-def test_delete_files_success():
-    key = "test_key12"
-    encoded_content = ["encoded_content_1", "encoded_content_2"]
-
-    client.post("/upload/", data={"key": key, "encoded_content": encoded_content})
+#test for delete sucess
+    key = "test_key"
 
     response = client.delete(f"/delete/?key={key}")
     assert response.status_code == 200
     assert response.json() == {"message": f"Folder '{key}' and its contents deleted successfully"}
-
-def test_delete_files_no_key_found():
+#test for failure of delete
     non_existent_key = "non_existent_key"
 
     response = client.delete(f"/delete/?key={non_existent_key}")
