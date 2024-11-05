d9280564164d5916ff611e49ea1b067adb182e8a
noobed-max <md.siddiq.romazan@gmail.com>
2024-11-05 21:35:23 +0530

    few changes

--- CIAOS-Rust/client/ciaos/client.py
+++ CIAOS-Rust/client/ciaos/client.py
@@ -1,6 +1,4 @@
-import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), 'util'))
 import requests
 from typing import List
 from util.flatbuffer.flatbuffer_handler import create_flatbuffer, parse_flatbuffer
