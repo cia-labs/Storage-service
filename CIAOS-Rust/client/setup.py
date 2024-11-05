995d43003f9851e74591b3d964aa2f32884260a4
noobed-max <md.siddiq.romazan@gmail.com>
2024-11-05 13:02:54 +0530

    updated version

--- CIAOS-Rust/client/setup.py
+++ CIAOS-Rust/client/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ciaos',
-    version='0.1.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         "flatbuffers>=24.3.25",
